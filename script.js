702a86f776e2e4fc62a519c3219de06f0040ac02
rus733 <9282716617@mail.ru>
2021-12-22 19:41:07 +0300

    материал лекции 19

---
+++ script.js
@@ -0,0 +1,107 @@
+//console.log(window);
+//window.setTimeout(() => {
+// console.log('привет я setTimeout');
+//}, 2000);
+//
+//setTimeout(() => {
+//window не обязательно
+//  console.log('привет я setTimeout');
+//}, 2000);
+//////////////////////////
+//const logger = () => {
+//console.log('привет я setTimeout');
+//};
+
+//setTimeout(logger, 2000); //метод вызывает logger, поэтому нет скобок
+/////////////////////////
+//ФУНКЦИЯ обертка  , если нужно работать с параметрами
+
+/*
+const logger = (str) => {
+  console.log(`Привет, я ${str}`);
+};
+
+//setTimeout(() => {
+//logger('setTimeout');
+//}, 2000);
+
+let idTimeout = setTimeout(() => {
+  logger('setTimeout');
+}, 2000);
+
+clearTimeout(idTimeout); //методы очистки
+
+/////////////////
+//метод setInterval
+setInterval(() => {
+  logger('setInterval');
+}, 2000);
+*/
+
+/*
+//включатель , выключатель 
+
+//let count = 0;
+let active = false;
+let idTimeout;
+document.addEventListener('click', () => {
+  if (active) {
+    active = false;
+  } else {
+    active = true;
+  }
+
+  logger('setTimeout');
+  //console.log(active);
+});
+
+const logger = (str) => {
+  if (active) {
+    console.log(`Привет, я ${str}`);
+    idTimeout = setTimeout(() => {
+      logger('setTimeout');
+    }, 500);
+  } else {
+    clearTimeout(idTimeout);
+  }
+};
+
+//clearTimeout(idTimeout);
+*/
+/*
+///очистить интервол по счетчику
+let idInterval = setInterval(() => {
+  count++;
+  logger('setInterval');
+
+  if (count === 10) clearInterval(idInterval);
+}, 500);
+*/
+
+/// упростим  включатель выключатель
+
+//let count = 0;
+let active = false;
+let idTimeout;
+document.addEventListener('click', () => {
+  //if (active) {
+  //  active = false;
+  //} else {
+  //  active = true;
+  //}
+  active = !active;
+
+  logger('setTimeout');
+  //console.log(active);
+});
+
+const logger = (str) => {
+  if (active) {
+    console.log(`Привет, я ${str}`);
+    idTimeout = setTimeout(() => {
+      logger('setTimeout');
+    }, 500);
+  } else {
+    clearTimeout(idTimeout);
+  }
+};
